571194a3f17230bdcc064c79bac7f9cacf788945
Oliver Hathaway <oliver@labiraus.com>
2024-09-05 11:22:39 +0100

    Fixed missing assignment

--- api.go
+++ api.go
@@ -7,9 +7,7 @@ import (
 	"net/http"
 )
 
-type apiBuffer struct {
-	requests chan<- apiRequest
-}
+var requestBuffer chan<- apiRequest
 
 type apiRequest struct {
 	verb     string
@@ -20,19 +18,18 @@ type apiRequest struct {
 
 func startApi(ctx context.Context) <-chan struct{} {
 	requests := make(chan apiRequest, 10)
-	buffer := apiBuffer{requests: requests}
-
+	requestBuffer = requests
 	done := make(chan struct{})
 	// middleware
 	// authentication
 	srv := &http.Server{Addr: "0.0.0.0:8080"}
-	http.HandleFunc("/", buffer.handle())
+	http.HandleFunc("/", handle)
 	go func() {
 		if err := srv.ListenAndServe(); err != http.ErrServerClosed {
 			panic("ListenAndServe: " + err.Error())
 		}
 	}()
-	loopDone := processingLoop(requests, ctx)
+	loopDone := actor(requests, ctx)
 	go func() {
 		defer close(done)
 
@@ -46,27 +43,39 @@ func startApi(ctx context.Context) <-chan struct{} {
 	return done
 }
 
-func processingLoop(requests chan apiRequest, ctx context.Context) <-chan struct{} {
+func actor(requests chan apiRequest, ctx context.Context) <-chan struct{} {
 	done := make(chan struct{})
+	// Shutdown
+	go func() {
+		<-ctx.Done()
+		close(requests)
+	}()
+
 	go func() {
 		defer close(done)
-		go func() {
-			<-ctx.Done()
-			close(requests)
-		}()
-		data := make(map[string][]byte)
+		processLoop(requests)
+	}()
+
+	return done
+}
+
+func processLoop(requests <-chan apiRequest) <-chan struct{} {
+	done := make(chan struct{})
+	go func() {
+		store := make(map[string][]byte)
+		defer close(done)
 		for req := range requests {
 			switch req.verb {
 			case http.MethodDelete:
-				delete(data, req.key)
+				delete(store, req.key)
 
 			case http.MethodPut:
 			case http.MethodPost:
 			case http.MethodPatch:
-				data[req.key] = req.value
+				store[req.key] = req.value
 
 			case http.MethodGet:
-				value, ok := data[req.key]
+				value, ok := store[req.key]
 				if !ok {
 					log.Printf("could not find value for key %v", req.key)
 				} else {
@@ -79,51 +88,47 @@ func processingLoop(requests chan apiRequest, ctx context.Context) <-chan struct
 	return done
 }
 
-func (buffer *apiBuffer) handle() func(http.ResponseWriter, *http.Request) {
-	return func(rw http.ResponseWriter, r *http.Request) {
-		var err error
-		defer func() {
-			r := recover()
-			if r != nil {
-				log.Println(err)
-				log.Println(r)
-				rw.WriteHeader(http.StatusInternalServerError)
-			}
-		}()
-
-		responseChan := make(chan []byte)
-		request := apiRequest{verb: r.Method, key: r.URL.Path, response: responseChan}
-		if r.Method != http.MethodGet && r.Method != http.MethodDelete {
-			body, err := io.ReadAll(io.Reader(r.Body))
-			defer r.Body.Close()
-			if err != nil {
-				log.Println(err)
-				rw.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			request.value = body
-		}
-
-		select {
-		case buffer.requests <- request:
-		default:
-			log.Println("request buffer full or shutdown")
-			rw.WriteHeader(http.StatusServiceUnavailable)
-			return
-		}
-
-		response, ok := <-responseChan
-
-		if r.Method != http.MethodGet {
-			rw.WriteHeader(http.StatusOK)
-			return
-		}
-
-		if ok {
-			rw.WriteHeader(http.StatusOK)
-			rw.Write(response)
-		} else {
+func handle(rw http.ResponseWriter, r *http.Request) {
+	defer func() {
+		r := recover()
+		if r != nil {
+			log.Println(r)
 			rw.WriteHeader(http.StatusInternalServerError)
 		}
+	}()
+
+	responseChan := make(chan []byte)
+	request := apiRequest{verb: r.Method, key: r.URL.Path, response: responseChan}
+	if r.Method != http.MethodGet && r.Method != http.MethodDelete {
+		body, err := io.ReadAll(io.Reader(r.Body))
+		defer r.Body.Close()
+		if err != nil {
+			log.Println(err)
+			rw.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		request.value = body
+	}
+
+	select {
+	case requestBuffer <- request:
+	default:
+		log.Println("request buffer full or shutdown")
+		rw.WriteHeader(http.StatusServiceUnavailable)
+		return
+	}
+
+	response, ok := <-responseChan
+
+	if r.Method != http.MethodGet {
+		rw.WriteHeader(http.StatusOK)
+		return
+	}
+
+	if ok {
+		rw.WriteHeader(http.StatusOK)
+		rw.Write(response)
+	} else {
+		rw.WriteHeader(http.StatusInternalServerError)
 	}
 }
